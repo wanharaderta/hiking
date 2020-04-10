f5028da5f5b169fad6a450de002cc67e2de51d5c
wanhar <wanhar@sehatq.com>
2020-04-10 22:38:28 +0700

    Initial Commit

---
+++ Hiking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hiking
+//
+//  Created by Wanhar on 10/04/20.
+//  Copyright © 2020 Wanhar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HikingTests/HikingTests.swift
@@ -0,0 +1,34 @@
+//
+//  HikingTests.swift
+//  HikingTests
+//
+//  Created by Wanhar on 10/04/20.
+//  Copyright © 2020 Wanhar. All rights reserved.
+//
+
+import XCTest
+@testable import Hiking
+
+class HikingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
